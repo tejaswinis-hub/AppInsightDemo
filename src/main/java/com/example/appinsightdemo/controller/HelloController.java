4109fb66f2bac5fe5102d758e82a018c6dcbcc53
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-06-25 18:41:03 +0530

    bug fix

--- src/main/java/com/example/appinsightdemo/controller/HelloController.java
+++ src/main/java/com/example/appinsightdemo/controller/HelloController.java
@@ -1,14 +1,15 @@
-package controller;
+package com.example.appinsightdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/welcom")
-public class AppInsightController {
-    @GetMapping(path = "/home")
-    public String say() {
-        return "Welcome to java world";
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
     }
 }
