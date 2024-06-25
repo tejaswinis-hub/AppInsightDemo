3ebe91d5968701287d63254475afb6ba65722836
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-06-25 17:48:18 +0530

    second commit

--- src/main/java/controller/AppInsightController.java
+++ src/main/java/controller/AppInsightController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppInsightController {
     @GetMapping(path = "/home")
     public String say() {
-        return "Welcome to java";
+        return "Welcome to java world";
     }
 }
