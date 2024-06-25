63911d4c9824ebd5b42706a515e578388a5a917c
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-06-25 19:57:34 +0530

    telemetry

--- src/main/java/com/example/appinsightdemo/controller/HelloController.java
+++ src/main/java/com/example/appinsightdemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.appinsightdemo.controller;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,13 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
+        TelemetryClient telemetryClient = new TelemetryClient();
+        RequestTelemetry requestTelemetry = new RequestTelemetry();
+        requestTelemetry.getProperties().put("TenantId", "tenantId");
+        telemetryClient.trackRequest(requestTelemetry);
+        telemetryClient.trackTrace("custom metric");
+        telemetryClient.flush();
+
         return "Hello,Java World!";
     }
 }
