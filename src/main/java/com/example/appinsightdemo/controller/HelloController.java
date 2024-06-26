6e2fc5ee24917609c82e9078e8f768408907658d
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-06-26 17:42:45 +0530

    added new api

--- src/main/java/com/example/appinsightdemo/controller/HelloController.java
+++ src/main/java/com/example/appinsightdemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.appinsightdemo.controller;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +13,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello,Java World!";
+    }
+
+    @GetMapping("/hello1")
+    public String sayHello1() {
+        TelemetryClient telemetryClient = new TelemetryClient();
+        RequestTelemetry requestTelemetry = new RequestTelemetry();
+        // Add custom properties
+        requestTelemetry.getProperties().put("traceId", "678trace");
+        telemetryClient.trackRequest(requestTelemetry);
+
+        return "Hello,Python World!";
     }
 }
