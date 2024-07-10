4de6cac429bd95f7e952cd54480817296550a98b
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-07-10 12:24:10 +0530

    removed exception catch

--- src/main/java/com/example/appinsightdemo/config/RequestTelemetryFilter.java
+++ src/main/java/com/example/appinsightdemo/config/RequestTelemetryFilter.java
@@ -3,6 +3,7 @@ package com.example.appinsightdemo.config;
 import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -30,42 +31,42 @@ public class RequestTelemetryFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws IOException {
+            throws IOException, ServletException {
         logger.info("Entered inside doFilterInternal method");
-        // TelemetryClient telemetryClient = new TelemetryClient();
-        // TelemetryClient telemetryClient = TelemetryClientData.getInstance();
-
-        // Start custom request telemetry
-        RequestTelemetry requestTelemetry = new RequestTelemetry();
-        requestTelemetry.setName(request.getRequestURI());
-        requestTelemetry.setTimestamp(new java.util.Date());
-        requestTelemetry.setUrl(new URL(request.getRequestURL().toString()));
-
-        // Add custom properties
-        String tenantId = request.getHeader("tenantId");
-        String contactId = request.getHeader("contactId");
-        String traceId = request.getHeader("TraceId");
-        requestTelemetry.getProperties().put("TenantId", tenantId);
-        requestTelemetry.getProperties().put("ContactId", contactId);
-        requestTelemetry.getProperties().put("TraceId", traceId);
-        logger.info("Added Properties");
-
-
-        // Track the custom request telemetry
-        // telemetryClient.getContext().setConnectionString("InstrumentationKey=bf0d9b88-d94c-42f2-b638-e086fd4807c3;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=4a72099e-5e0a-4ec6-ba60-35f1bd6cc23d");
-
-        logger.info("ConnectionString:" + TELEMETRY_CLIENT.getContext().getConnectionString());
-        logger.info("Connection String from request telemetry:" + requestTelemetry.getContext().getConnectionString());
-        logger.info("Connection String from request telemetry:" + requestTelemetry.getContext().getInstrumentationKey());
-
-        TELEMETRY_CLIENT.trackRequest(requestTelemetry);
-
         try {
+            // TelemetryClient telemetryClient = new TelemetryClient();
+            // TelemetryClient telemetryClient = TelemetryClientData.getInstance();
+
+            // Start custom request telemetry
+            RequestTelemetry requestTelemetry = new RequestTelemetry();
+            requestTelemetry.setName(request.getRequestURI());
+            requestTelemetry.setTimestamp(new java.util.Date());
+            requestTelemetry.setUrl(new URL(request.getRequestURL().toString()));
+
+            // Add custom properties
+            String tenantId = request.getHeader("tenantId");
+            String contactId = request.getHeader("contactId");
+            String traceId = request.getHeader("TraceId");
+            requestTelemetry.getProperties().put("TenantId", tenantId);
+            requestTelemetry.getProperties().put("ContactId", contactId);
+            requestTelemetry.getProperties().put("TraceId", traceId);
+            logger.info("Added Properties");
+
+
+            // Track the custom request telemetry
+            // telemetryClient.getContext().setConnectionString("InstrumentationKey=bf0d9b88-d94c-42f2-b638-e086fd4807c3;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=4a72099e-5e0a-4ec6-ba60-35f1bd6cc23d");
+
+            logger.info("ConnectionString:" + TELEMETRY_CLIENT.getContext().getConnectionString());
+            logger.info("Connection String from request telemetry:" + requestTelemetry.getContext().getConnectionString());
+            logger.info("Connection String from request telemetry:" + requestTelemetry.getContext().getInstrumentationKey());
+
+            TELEMETRY_CLIENT.trackRequest(requestTelemetry);
+
+
             filterChain.doFilter(request, response);
         } catch (Exception e) {
             logger.info("Failed to add custom properties");
-            resolver.resolveException(request, response, null, e);
-
+            filterChain.doFilter(request, response);
         }
     }
 
