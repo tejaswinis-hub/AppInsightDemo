f6ab05df03cec3835b775b05a5cd3093c8609fad
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-07-10 11:01:48 +0530

    Created telemetry client constant

--- src/main/java/com/example/appinsightdemo/config/RequestTelemetryFilter.java
+++ src/main/java/com/example/appinsightdemo/config/RequestTelemetryFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
@@ -15,11 +15,13 @@ import java.net.URL;
 import java.util.logging.Logger;
 
 
-@Component
+@Service
 public class RequestTelemetryFilter extends OncePerRequestFilter {
     private static final Logger logger = Logger.getLogger(RequestTelemetryFilter.class.getName());
 
     private final HandlerExceptionResolver resolver;
+    private static final TelemetryClient TELEMETRY_CLIENT = new TelemetryClient();
+
 
     public RequestTelemetryFilter(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.resolver = resolver;
@@ -31,7 +33,7 @@ public class RequestTelemetryFilter extends OncePerRequestFilter {
             throws IOException {
         logger.info("Entered inside doFilterInternal method");
         // TelemetryClient telemetryClient = new TelemetryClient();
-        TelemetryClient telemetryClient = TelemetryClientData.getInstance();
+        // TelemetryClient telemetryClient = TelemetryClientData.getInstance();
 
         // Start custom request telemetry
         RequestTelemetry requestTelemetry = new RequestTelemetry();
@@ -52,11 +54,11 @@ public class RequestTelemetryFilter extends OncePerRequestFilter {
         // Track the custom request telemetry
         // telemetryClient.getContext().setConnectionString("InstrumentationKey=bf0d9b88-d94c-42f2-b638-e086fd4807c3;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=4a72099e-5e0a-4ec6-ba60-35f1bd6cc23d");
 
-        logger.info("ConnectionString:" + telemetryClient.getContext().getConnectionString());
+        logger.info("ConnectionString:" + TELEMETRY_CLIENT.getContext().getConnectionString());
         logger.info("Connection String from request telemetry:" + requestTelemetry.getContext().getConnectionString());
         logger.info("Connection String from request telemetry:" + requestTelemetry.getContext().getInstrumentationKey());
 
-        telemetryClient.trackRequest(requestTelemetry);
+        TELEMETRY_CLIENT.trackRequest(requestTelemetry);
 
         try {
             filterChain.doFilter(request, response);
