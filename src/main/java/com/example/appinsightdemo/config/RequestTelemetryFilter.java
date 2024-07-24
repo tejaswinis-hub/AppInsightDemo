6f4a2d19d74bbdb7a2991f23725f44ae42e890c9
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-07-24 21:12:39 +0530

    refactored the code

--- src/main/java/com/example/appinsightdemo/config/RequestTelemetryFilter.java
+++ src/main/java/com/example/appinsightdemo/config/RequestTelemetryFilter.java
@@ -20,8 +20,6 @@ public class RequestTelemetryFilter extends OncePerRequestFilter {
     private static final Logger logger = Logger.getLogger(RequestTelemetryFilter.class.getName());
 
     private static final TelemetryClient TELEMETRY_CLIENT = new TelemetryClient();
-    private final AtomicInteger metricValue = new AtomicInteger();
-
     private static final String TENANT_ID = "TenantId";
     private static final String CONTACT_ID = "ContactId";
     private static final String TRACE_ID = "TraceId";
@@ -38,13 +36,13 @@ public class RequestTelemetryFilter extends OncePerRequestFilter {
         try {
             //to clear azure default custom properties
             TELEMETRY_CLIENT.getContext().getProperties().clear();
+
             // Start custom request telemetry
             RequestTelemetry requestTelemetry = new RequestTelemetry();
             //RequestTelemetry requestTelemetry = new RequestTelemetry("API details", null, null, null, true);
 
 
             // Add custom properties
-
             String tenantId = request.getHeader("tenantId");
             String contactId = request.getHeader("contactId");
             String traceId = request.getHeader("traceId");
@@ -53,17 +51,9 @@ public class RequestTelemetryFilter extends OncePerRequestFilter {
             requestTelemetry.getProperties().put(CONTACT_ID, contactId);
             requestTelemetry.getProperties().put(TRACE_ID, traceId);
 
-            /*Map<String, String> requestTelemetry = new HashMap<>();
-            requestTelemetry.put(TENANT_ID, tenantId);
-            requestTelemetry.put(CONTACT_ID, contactId);
-            requestTelemetry.put(TRACE_ID, traceId);*/
             logger.info("Added Properties");
 
-
             TELEMETRY_CLIENT.trackRequest(requestTelemetry);
-            // TELEMETRY_CLIENT.getContext().getProperties().clear();
-            //track(requestTelemetry);
-
 
         } catch (Exception e) {
             logger.info("Failed to add custom properties");
@@ -71,38 +61,4 @@ public class RequestTelemetryFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         }
     }
-
-    private void track(Map<String, String> requestTelemetry) {
-        // value represents the sum of the individual metric values in the sample.
-        // Because the sample size is 1, value is equal to the metricValue.
-        double value = metricValue.get();
-
-        // sampleCount represents a count of the individual values in the sample.
-        // Because the sample size is 1, sampleCount is always 1.
-        int sampleCount = 1;
-
-        // min represents the minimum value of the sample.
-        // Because the sample size is 1, min is equal to the metricValue.
-        double min = metricValue.get();
-
-        // max represents the minimum value of the sample.
-        // Because the sample size is 1, max is equal to the metricValue.
-        double max = metricValue.get();
-
-        // The standard deviation of the sample. When stdDev is 0, all the values in the sample are identical.
-        // Because the sample size is 1, stdDev is always 0.
-        double stdDev = 0;
-
-
-        TELEMETRY_CLIENT.trackMetric("Custom properties", value,
-                sampleCount,
-                min,
-                max,
-                stdDev,
-                requestTelemetry);
-        logger.info("Added custom properties to track metric");
-
-    }
-
-
 }
