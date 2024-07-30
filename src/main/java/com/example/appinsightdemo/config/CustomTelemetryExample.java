3a189c7619d14f24c1a703d62b60babc35afab50
tejaswini.shivakumar <tejaswini.shivakumar@mobileheartbeat.com>
2024-07-30 13:22:10 +0530

    Added another logic

--- src/main/java/com/example/appinsightdemo/config/CustomTelemetryExample.java
+++ src/main/java/com/example/appinsightdemo/config/CustomTelemetryExample.java
@@ -9,7 +9,10 @@ public class CustomTelemetryExample {
     private static final TelemetryClient telemetryClient = ApplicationInsightsInitializer.getTelemetryClient();
 
     public static void logInitialEvent() {
-       
+        //Initialize event
+        String eventName = "InitialEvent";
+        telemetryClient.trackEvent(eventName);
+
         // Add custom properties
         TelemetryContext context = telemetryClient.getContext();
         context.getProperties().put("CustomContextProperty", "ContextValue");
